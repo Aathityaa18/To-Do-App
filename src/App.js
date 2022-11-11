e3cbfa616377be29a39116f421fd9cb9bd338f03
Aathityaa G R <107313820+Aathityaa18@users.noreply.github.com>
2022-11-11 07:01:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className='container border border-primary rounded mt-5'
     style={{
-      background: `url(https://images.unsplash.com/photo-1602640572232-7345ff5f752d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTgzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60)`,
+      background: `url(https://images.unsplash.com/photo-1489875347897-49f64b51c1f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzh8fG5vdGVib29rfGVufDB8MHwwfGJsYWNrfA%3D%3D&auto=format&fit=crop&w=600&q=60)`,
       backgroundSize: 'cover'
     }}
     >
